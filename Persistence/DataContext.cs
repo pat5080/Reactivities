470d02a350d4e3df2a6410583c1b61a4daca37db
Patrick Korczak <patrick5080@hotmail.com>
2022-07-04 21:18:19 +1000

    API setup complete

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Activity> Activities {get; set;}
+    }
+}
