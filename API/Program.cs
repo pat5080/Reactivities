bb3b782dc38a7cf3c93f38878b4c613546c60d87
Patrick Korczak <patrick.korczak@avanade.com>
2022-08-03 12:46:07 +1000

    11. Complete

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+/*
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+})
+*/
 
 var app = builder.Build();
 
